80bf27fb86da917e21aabc136e9b8eb8edfbcaa2
Khagesh1528 <104688846+Khagesh1528@users.noreply.github.com>
2023-07-21 20:15:48 +0530

    adding increase function

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,19 +1,49 @@
 import React from "react";
-import { render } from "react-dom";
+
 
 class CartItem extends React.Component{
+  
+    constructor () {
+        super();
+        this.state = {
+           price:999,
+           title:"Mobile Phone",
+           Qty:1,
+           img:''
+
+        }
+    }
+    increaseQuantity =()=>{
+      console.log('this',this.state);
+    }
     render(){
+        const{price,title,Qty} = this.state;
         return(
         <div className="cart-item">
                 <div className="left-block">
                 <img style={Styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{ color:'#777' }}>Rs 999</div>
-                    <div style={{ color:'#777' }}>QTY: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{ color:'#777' }}>Rs {price}</div>
+                    <div style={{ color:'#777' }}>QTY: {Qty}</div>
                 </div>
                 <div className="cart-item-actions">
+
+                    <img alt="increase" onClick={this.increaseQuantity}
+                    className="action-icons" 
+                    src="https://cdn-icons-png.flaticon.com/128/3303/3303893.png" 
+                    />
+
+                    <img alt="decrease" 
+                    className="action-icons" 
+                    src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png"
+                    />
+                    
+                    <img alt="delete" 
+                    className="action-icons" 
+                    src="https://cdn-icons-png.flaticon.com/128/484/484611.png" 
+                    />
 
                 </div>
         </div>
